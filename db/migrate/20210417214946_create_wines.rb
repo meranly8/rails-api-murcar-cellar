4618a07a268f68fa9580fc7787069bf7a979b08d
Michelle Ranly <meranly8@gmail.com>
2021-04-17 16:56:08 -0500

    Resource generator files added

---
+++ db/migrate/20210417214946_create_wines.rb
@@ -0,0 +1,17 @@
+class CreateWines < ActiveRecord::Migration[6.1]
+  def change
+    create_table :wines do |t|
+      t.string :wine
+      t.string :region
+      t.string :country
+      t.integer :year
+      t.boolean :opened
+      t.string :image
+      t.float :price
+      t.integer :quantity
+      t.float :rating
+
+      t.timestamps
+    end
+  end
+end

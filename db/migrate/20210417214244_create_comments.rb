1c4db4315e13162ee234059be4c883e98ccc39e6
Michelle Ranly <meranly8@gmail.com>
2021-04-17 17:14:58 -0500

    Updated comment field to be text

--- db/migrate/20210417214244_create_comments.rb
+++ db/migrate/20210417214244_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.string :name
-      t.string :comment
+      t.text :comment
       t.integer :wine_id
 
       t.timestamps

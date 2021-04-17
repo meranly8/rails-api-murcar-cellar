c5bfeecafbeba0bf908f4a307af7f45fab85a2a3
Michelle Ranly <meranly8@gmail.com>
2021-04-17 17:03:55 -0500

    Added wine_id into Comments table

--- db/migrate/20210417214244_create_comments.rb
+++ db/migrate/20210417214244_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
     create_table :comments do |t|
       t.string :name
       t.string :comment
+      t.integer :wine_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2021_04_17_214946) do
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.string "comment"
+    t.integer "wine_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

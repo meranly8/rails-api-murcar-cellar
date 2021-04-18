567f2aa84a4930003d614f099271e9845cac0ff0
Michelle Ranly <meranly8@gmail.com>
2021-04-17 19:41:26 -0500

    Defined wines#show & added has_many :comments into WineSerializer

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -3,4 +3,9 @@ class WinesController < ApplicationController
         wines = Wine.all
         render json: WineSerializer.new(wines)
     end
+
+    def show
+        wine = Wine.find_by(id: params[:id])
+        render json: WineSerializer.new(wine, include: [:comments])
+    end
 end
--- app/serializers/wine_serializer.rb
+++ app/serializers/wine_serializer.rb
@@ -1,5 +1,6 @@
 class WineSerializer
   include FastJsonapi::ObjectSerializer
   
+  has_many :comments
   attributes :wine, :region, :country, :year, :opened, :image, :price, :rating
 end

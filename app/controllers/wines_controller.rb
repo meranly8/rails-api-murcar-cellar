fb88596d60e1331ac675887941ad4bc4c9e846f1
Michelle Ranly <meranly8@gmail.com>
2021-04-20 21:51:11 -0500

    Defined wines#create & wines#wine_params

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -8,4 +8,19 @@ class WinesController < ApplicationController
         wine = Wine.find_by(id: params[:id])
         render json: WineSerializer.new(wine, include: [:comments])
     end
+
+    def create
+        wine = Wine.new(wine_params)
+        if wine.save
+            render json: WineSerializer.new(wine)
+        else
+            render json: {invalid: "Error occurred when trying to save wine"}
+        end
+    end
+
+
+    private
+        def wine_params
+            params.require(:wine).permit(:wine, :region, :country, :year, :price, :image, :opened, :rating)
+        end
 end

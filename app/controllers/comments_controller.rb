bf4f24c2ac8ad12f16462e4762fa4127e496b0ec
Michelle Ranly <meranly8@gmail.com>
2021-04-25 22:10:26 -0500

    Defined comments#create & comment#comment_params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,4 +8,18 @@ class CommentsController < ApplicationController
         comment = Comment.find_by(id: params[:id])
         render json: CommentSerializer.new(comment, include: [:wine])
     end
+
+    def create
+        comment = Comment.new(comment_params)
+        if comment.save
+            render json: CommentSerializer.new(comment)
+        else
+            render json: {invalid: "Error occurred when trying to save comment"}
+        end
+    end
+
+    private
+        def comment_params
+            params.permit(:name, :comment, :wine_id)
+        end
 end

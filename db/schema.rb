147565d15d1706b8c71f8dc586672a95d0c0792a
Michelle Ranly <meranly8@gmail.com>
2021-05-04 00:06:42 -0400

    add_column :wines, :quantity, :integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_214946) do
+ActiveRecord::Schema.define(version: 2021_05_04_040357) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_04_17_214946) do
     t.float "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "quantity"
   end
 
 end
